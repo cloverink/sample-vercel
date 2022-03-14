ac1ed8786a707b3e8cd9a95249cec1f6ce4901cb
Aran Chananar <cloverink@gmail.com>
2022-03-14 12:07:29 +0700

    test(env): test calling env from config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    foo: process.env.FOO,
+    start_server: new Date().toISOString()
+  }
 }
 
 module.exports = nextConfig
