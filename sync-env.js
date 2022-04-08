cefc49e6a417e46817b30f328f0172c135b4c9b6
Aran Chananar <cloverink@gmail.com>
2022-04-08 16:47:30 +0700

    ci(env): need to check NPM_TOKEN

--- sync-env.js
+++ sync-env.js
@@ -4,6 +4,7 @@ const run = () => {
   log.info('========= sync-env =========')
   log.info('process.env.foo=', process.env.foo)
   log.info('process.env.hello=', process.env.hello)
+  console.log(process.env)
 }
 
 run()
